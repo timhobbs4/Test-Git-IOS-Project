5e8671821895b95e397aabf22fe9914de1d6ab05
Tim Hobbs <timhobbs@Tims-MacBook-Pro.local>
2014-11-01 09:44:57 +1000

    added a test class

---
+++ THTest.h
@@ -0,0 +1,13 @@
+//
+//  THTest.h
+//  Git Test
+//
+//  Created by Tim Hobbs on 1/11/2014.
+//  Copyright (c) 2014 Tim Hobbs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface THTest : NSObject
+
+@end
---
+++ THTest.m
@@ -0,0 +1,13 @@
+//
+//  THTest.m
+//  Git Test
+//
+//  Created by Tim Hobbs on 1/11/2014.
+//  Copyright (c) 2014 Tim Hobbs. All rights reserved.
+//
+
+#import "THTest.h"
+
+@implementation THTest
+
+@end
